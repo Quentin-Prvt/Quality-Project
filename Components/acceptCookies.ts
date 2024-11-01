a245470618a7e3b61bcd5cf4044ae43d61264800
Maxence DE POORTER <90178284+Maxence-De-Poorter@users.noreply.github.com>
2024-11-01 16:41:44 +0100

    Update acceptCookies.ts

--- Components/acceptCookies.ts
+++ Components/acceptCookies.ts
@@ -1,9 +1,16 @@
 import {Page} from "@playwright/test";
 
-export async function acceptCookies(page:Page) {
+export async function acceptCookies(page: Page) {
+    const cookieSelector = '#sp-cc-accept';
     try {
-        await page.click('#sp-cc-accept'); //this accepts cookies
+        // Vérifie si le bouton d'acceptation des cookies est présent
+        if (await page.$(cookieSelector)) {
+            await page.click(cookieSelector); // Accepte les cookies si le bouton est visible
+            console.log('Cookies accepted.');
+        } else {
+            console.log('Cookie acceptance button not found or already accepted.');
+        }
     } catch (error) {
-        console.log('Cookie acceptance button not found or already accepted.');
+        console.log('An error occurred while trying to accept cookies:', error);
     }
 }
