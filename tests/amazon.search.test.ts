9b9163cb08faa146cdd523c49c953b83dfd878c9
Maxence DE POORTER <90178284+Maxence-De-Poorter@users.noreply.github.com>
2024-11-04 08:46:41 +0100

    Fixing Search by category expect and fixing antibot problem

--- tests/amazon.search.test.ts
+++ tests/amazon.search.test.ts
@@ -9,21 +9,25 @@ test.describe('Amazon Search', () => {
     });
 
     test('Search by Category', async ({ page }) => {
+        await acceptCookies(page);
+        // Open the category menu and click on "Livres"
         await page.getByLabel('Ouvrir le menu Toutes les cat').click();
-        //cliquer sur la catégorie "Livres"
         await page.locator('a.hmenu-item[data-menu-id="9"]').filter({ hasText: 'Livres' }).nth(0).click();
-        //cliquer sur la catégorie "Livres en français"
+
+        // Click on "Tous les livres" within the "Livres" category
         await page.getByRole('link', { name: 'Tous les livres' }).nth(1).click();
-        // cliquer sur le premier lvire dans les nouveautés
+
+        // Click on the first book in the "Nouveautés" section
         await page.getByRole('link', { name: 'Voir plus Nouveautés' }).click();
         await page.locator('#p13n-asin-index-0').click();
-        //fait moi un except pour finaliser le test par rapport au test que j'ai fait, pas de to have title et pas avec le h1
-        await expect(page.locator('h1').first()).toHaveText('    Ce que je cherche     Broché – 9 novembre 2024   ');
+
+        // Verify that the product page has loaded by checking for the presence of a stable element
+        await expect(page.locator('#productTitle')).toBeVisible(); // Checks for product title element
     });
 
     test('Search by Keyword', async ({ page }) => {
         // Perform a search for a keyword (e.g., "smartphone")
-        await page.fill('input[name="field-keywords"]', 'smartphone');
+        await page.fill('#twotabsearchtextbox', 'smartphone');
         await page.click('input#nav-search-submit-button');
 
         // Wait for search results to load
